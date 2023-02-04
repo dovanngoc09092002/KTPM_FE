eabed18a96b6b44a230ec2c76d5da7f6388bdf74
dovanngoc09092002 <phamminhvien9922@gmail.com>
2023-02-04 10:46:07 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/reducers"
+import mySaga from './redux/sagas';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension"
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+// import { PersistGate } from 'redux-persist/lib/integration/react';
+
+
+// const persistConfig = {
+//   key: 'root',
+//   storage: storage,
+//   stateReconciler: autoMergeLevel2 // Xem thêm tại mục "Quá trình merge".
+// };
+
+
+// const pReducer = persistReducer(persistConfig, rootReducer);
+
+
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+// const persistor = persistStore(store);
+
+sagaMiddleware.run(mySaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    {/* <PersistGate loading = { null } persistor={persistor}> */}
+      <App />
+    {/* </PersistGate> */}
+
+  </Provider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
